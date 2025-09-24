b73718f28ece8b02a8aa761018781f3f59866c24
Yukishida <yukisempai625@gmail.com>
2025-09-24 12:52:15 +0700

    Create project

---
+++ box_cox_transformation.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_diabetes
+import pandas as pd
+
+data = load_diabetes()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df["target"] = data.target
+
+print(df.head())
